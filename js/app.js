561a1c8d4cb7ca857aa1df207355c45a99614e1a
Kevin Luong <k5luong@uwaterloo.ca>
2016-11-13 09:35:53 -0700

    added about text and img

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
 $(document).ready(function(){
-  $('.scrolling').css('max-width', '100%');
-  $('#intro').hide();
+  $('.scrolling').animate({
+    'max-width': '75%',
+    }, 750, 'linear', function() {
+      $('#intro').fadeOut(500);
+  });
+});
+
+
+//Event handlers
+
+$('#clickable').click(function(){
+  $('nav').toggleClass('static');
+  $(this).next().toggleClass('flexed');
 });
