0443db16fa9a5d4142a9447b6d50a63f64047c6f
Kevin Luong <k5luong@uwaterloo.ca>
2017-09-26 11:38:13 -0400

    remove WIP features

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 $('.home').on('click', '.interests-header, .interests', (event) => {
   $('.interests').slideToggle(600);
+  $('.interests-dropdown').css('transform') !== 'none' 
+    ? $('.interests-dropdown').css('transform', 'none')
+    : $('.interests-dropdown').css('transform', 'rotate(90deg)')
 });
