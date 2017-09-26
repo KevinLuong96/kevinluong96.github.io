e13079bba4f15f0230bcf64eac1a6b12e8861ae7
Kevin Luong <k5luong@uwaterloo.ca>
2017-09-26 11:09:48 -0400

    added work experience

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
 $('.home').on('click', '.interests-header, .interests', (event) => {
   $('.interests').slideToggle(600);
 });
-
-$('.home').on('click', '.work-header, .work', (event) => {
-  $('.work').slideToggle(600);
-});
