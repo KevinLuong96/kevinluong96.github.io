66833c3c61846804cb3c1668e1a8880e6ac150fa
Kevin Luong <c1-kevin@correlation-one.com>
2018-05-22 00:43:19 -0400

    update 3.0

--- js/app.js
+++
@@ -1,6 +0,0 @@
-$('.home').on('click', '.interests-header, .interests', (event) => {
-  $('.interests').slideToggle(600);
-  $('.interests-dropdown').css('transform') !== 'none' 
-    ? $('.interests-dropdown').css('transform', 'none')
-    : $('.interests-dropdown').css('transform', 'rotate(90deg)')
-});
